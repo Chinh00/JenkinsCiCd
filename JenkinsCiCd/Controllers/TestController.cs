d67d2f8d91b4b9ada0db00e8d5d6c049f29d68b4
Chinh00 <94354399+Chinh00@users.noreply.github.com>
2024-04-15 18:44:26 +0700

    c

---
+++ JenkinsCiCd/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JenkinsCiCd.Controllers;
+
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> HandleTestAsync(CancellationToken cancellationToken)
+    {
+        return Ok(Task.FromResult("Hello world"));
+    }
+}
