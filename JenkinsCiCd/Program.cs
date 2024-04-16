390d9671234794d365687b4448d3ecf21166a7cf
Chinh00 <94354399+Chinh00@users.noreply.github.com>
2024-04-17 01:11:38 +0700

    Update Program.cs

--- JenkinsCiCd/Program.cs
+++ JenkinsCiCd/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,5 +17,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.MapControllers();
 app.Run();
