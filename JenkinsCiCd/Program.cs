40f21cb39f340b69e4f5f1ad0ad5c995878b9006
Chinh00 <94354399+Chinh00@users.noreply.github.com>
2024-04-17 01:01:14 +0700

    Update Program.cs

--- JenkinsCiCd/Program.cs
+++ JenkinsCiCd/Program.cs
@@ -15,9 +15,4 @@ if (app.Environment.IsDevelopment())
 }
 
 
-
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+app.Run();
